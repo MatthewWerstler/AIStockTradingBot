53376ddc66ef193ae328a46787c05cefa94328a5
Matthew Werstler <MatthewWerstler@users.noreply.github.com>
2019-12-29 16:27:37 -0500

    Adding and Testing basic Encryption in the logic library

---
+++ WorkingMansDayTradingTests/Logic/TestEncryption.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AIStockTradingBotLogic;
+
+namespace WorkingMansDayTradingTests.Logic
+{
+    [TestClass]
+    public class TestEncryption
+    {
+        [TestMethod]
+        public void shouldBeAbleToEncryptAndDecryptAString()
+        {
+            string EncryptionKey = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string testString = "Here is my string to test encrypting and decrypting with.";
+            string encrypted = Encryption.Encrypt(EncryptionKey, testString);
+            string decrypted = Encryption.Decrypt(EncryptionKey, encrypted);
+            Assert.AreEqual(testString, decrypted);
+            Assert.AreNotEqual(encrypted, testString);
+        }
+    }
+}
