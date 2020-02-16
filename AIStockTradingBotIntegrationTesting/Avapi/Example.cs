442e570a976148dc05d85962ca719889236ed1be
Matthew Werstler <MatthewWerstler@users.noreply.github.com>
2020-02-16 09:00:08 -0500

    Moving Avapi test from the console to the integration test project which was renamed to match master.

---
+++ AIStockTradingBotIntegrationTesting/Avapi/Example.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Avapi.AvapiTIME_SERIES_DAILY;
+using Avapi.AvapiTIME_SERIES_INTRADAY;
+using Avapi;
+
+namespace AvapiTest
+{
+    public class Example
+    {
+        public static IAvapiResponse_TIME_SERIES_DAILY_Content Test(string Api_Key)
+        { 
+            AvapiConnection connection = AvapiConnection.Instance;
+
+            // Set up the connection and pass the API_KEY provided by alphavantage.co
+            connection.Connect(Api_Key);
+
+            // Get the TIME_SERIES_DAILY query object
+            Int_TIME_SERIES_DAILY time_series_daily =
+                connection.GetQueryObject_TIME_SERIES_DAILY();
+
+            // Perform the TIME_SERIES_DAILY request and get the result
+            IAvapiResponse_TIME_SERIES_DAILY time_series_dailyResponse =
+            time_series_daily.Query(
+                 "MSFT",
+                 Const_TIME_SERIES_DAILY.TIME_SERIES_DAILY_outputsize.compact);
+
+            // Printout the results
+            //Console.WriteLine("******** RAW DATA TIME_SERIES_DAILY ********");
+            //Console.WriteLine(time_series_dailyResponse.RawData);
+
+            //Console.WriteLine("******** STRUCTURED DATA TIME_SERIES_DAILY ********");
+            var data = time_series_dailyResponse.Data;
+            if (data.Error)
+            {
+                //Console.WriteLine(data.ErrorMessage);
+                throw new Exception(data.ErrorMessage);
+            }
+            return data;
+        }
+
+        public static IAvapiResponse_TIME_SERIES_INTRADAY TestTimeByMinutes(string Api_Key)
+        {
+            AvapiConnection connection = AvapiConnection.Instance;
+
+            // Set up the connection and pass the API_KEY provided by alphavantage.co
+            connection.Connect(Api_Key);
+
+            // Get the  query object
+            Int_TIME_SERIES_INTRADAY intraday_series =
+                connection.GetQueryObject_TIME_SERIES_INTRADAY();
+
+
+            // Get the TIME_SERIES_DAILY query object
+            //Int_TIME_SERIES_DAILY time_series_daily =
+            //    connection.GetQueryObject_TIME_SERIES_DAILY();
+
+            // Perform the  request and get the result
+            IAvapiResponse_TIME_SERIES_INTRADAY time_series_IntradayResponse =
+                intraday_series.Query("EFC", Const_TIME_SERIES_INTRADAY.TIME_SERIES_INTRADAY_interval.n_1min, Const_TIME_SERIES_INTRADAY.TIME_SERIES_INTRADAY_outputsize.full);
+
+            return time_series_IntradayResponse;
+
+
+        }
+    }
+}
---
+++ AIStockTradingBotIntegrationTesting/Avapi/TestAvapi.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AvapiTest;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Linq;
+using Avapi.AvapiTIME_SERIES_INTRADAY;
+
+namespace WorkingMansDayTradingTests.Avapi
+{
+    [TestClass]
+    public class TestAvapi
+    {
+
+        private string alpha_vantage_key
+        {
+            get
+            {
+                var builder = new ConfigurationBuilder()
+                    .AddUserSecrets<UnitTestSecretsAndHttpClient>();
+                IConfiguration Configuration = builder.Build();
+                return Configuration["alpha_vantage_key"];
+            }
+        }
+
+        [TestMethod]
+        public void ShouldBeAbleToReturnSomethingFromAvapi()
+        {
+            IAvapiResponse_TIME_SERIES_INTRADAY efcIntradayData = Example.TestTimeByMinutes(alpha_vantage_key);
+            Assert.IsNotNull(efcIntradayData);
+            
+            var data = Example.Test(alpha_vantage_key);
+            //Console.WriteLine("Information: " + data.MetaData.Information);
+            //Console.WriteLine("Symbol: " + data.MetaData.Symbol);
+            //Console.WriteLine("LastRefreshed: " + data.MetaData.LastRefreshed);
+            //Console.WriteLine("OutputSize: " + data.MetaData.OutputSize);
+            //Console.WriteLine("TimeZone: " + data.MetaData.TimeZone);
+            //Console.WriteLine("========================");
+            //Console.WriteLine("========================");
+            //foreach (var timeseries in data.TimeSeries)
+            //{
+            //    Console.WriteLine("open: " + timeseries.open);
+            //    Console.WriteLine("high: " + timeseries.high);
+            //    Console.WriteLine("low: " + timeseries.low);
+            //    Console.WriteLine("close: " + timeseries.close);
+            //    Console.WriteLine("volume: " + timeseries.volume);
+            //    Console.WriteLine("DateTime: " + timeseries.DateTime);
+            //    Console.WriteLine("========================");
+            //}
+            Assert.IsNotNull(data.MetaData.Information);
+        }
+    }
+        
+}
