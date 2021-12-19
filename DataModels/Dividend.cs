00365c8aeeb6c5d284077f7b6d5e9443ae2a83c3
MatthewWerstler <matt@luminescentservices.com>
2021-12-19 01:21:26 -0500

    Dividend Data Model corrections.

---
+++ DataModels/Dividend.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModels
+{
+
+    public class Dividend
+    {
+        public string ticker { get; set; }
+        public string exDate { get; set; }
+        public string paymentDate { get; set; }
+        public string recordDate { get; set; }
+        public float amount { get; set; }
+    }
+}
