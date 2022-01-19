e3c83ddc3d2c66c77e808dc0a159eb02d0ccb01c
MatthewWerstler <matt@luminescentservices.com>
2022-01-19 18:22:08 -0500

    Adding QuoteFrequencies types (enum)

---
+++ DataModels/QuoteDetails.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModels
+{
+    public class QuoteDetails
+    {
+
+
+        public string StockTicker { get; set; }
+        public QuoteFrequencies QuoteFrequency { get; set; }
+        //public DateTime? FirstQuote { get; set; }
+        //public DateTime? LastQuote { get; set; }
+        public string FolderPath { get; set; }
+
+        /// <summary>
+        /// Standard constructor stock ticker and quoting frequency should be known during initialization
+        /// </summary>
+        /// <param name="stockTicker">Stock Market Symbol</param>
+        /// <param name="frequency">Quote frequency based on the defined enum list</param>
+        public QuoteDetails(string stockTicker, QuoteFrequencies frequency)
+        {
+            StockTicker = stockTicker;
+            QuoteFrequency = frequency;
+        }
+         
+
+    }
+
+    public enum QuoteFrequencies
+    {
+        ByMinute,
+        ByFifteenMinutes,
+        ByThirtyMinutes,
+        ByHour,
+        ByDay,
+        ByWeek,
+        ByMonth
+    }
+}
