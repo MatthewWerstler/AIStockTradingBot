f6948806cf94a91285fd50470efbeec95f1fcc31
MatthewWerstler <matt@luminescentservices.com>
2022-09-17 13:01:30 -0400

    StockQuoteDTO Data Module in DataModels.DTO namespace
    This data model works with Excel Stock Charts

---
+++ DataModels/DTO/StockQuoteDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DataModels.DTO
+{
+    public class StockQuoteDTO
+    {
+        public DateTime date { get; set; }
+        public double open { get; set; }
+        public double high { get; set; }
+        public double low { get; set; }
+        public double close { get; set; }
+    }
+}
