6dbb9a6ffc25401d1b4ed5a26888e625d1489d91
MatthewWerstler <matt@luminescentservices.com>
2023-11-14 19:10:06 -0500

    (untested) Polygon Aggregates Bars for testing

---
+++ Ploygon_Interface/API-Calls/StockAggregates.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Ploygon_Interface.API_Calls
+{
+    public class StockAggregates
+    {
+
+        public static HttpResponseMessage getStockDividendDates(HttpClient client, string APIKey, string StockTicker)
+        {
+            string apiURL = $"https://api.polygon.io/v2/aggs/ticker/{StockTicker}/range/1/minute/2023-10-09/2023-11-09?adjusted=false&sort=asc&limit=50000&apiKey={APIKey}";
+            return client.GetAsync(apiURL).Result;
+        }
+        
+    }
+}
