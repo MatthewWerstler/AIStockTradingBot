32a93ac7ddafe56e2432799ae4a13972bddf044d
MatthewWerstler <matt@luminescentservices.com>
2023-11-16 21:20:32 -0500

    Polygon Stock Minute Data

--- Ploygon_Interface/API-Calls/StockAggregates.cs
+++ Ploygon_Interface/API-Calls/StockAggregates.cs
@@ -8,7 +8,7 @@ namespace Ploygon_Interface.API_Calls
     public class StockAggregates
     {
 
-        public static HttpResponseMessage getStockDividendDates(HttpClient client, string APIKey, string StockTicker)
+        public static HttpResponseMessage getStockMinuteData(HttpClient client, string APIKey, string StockTicker)
         {
             string apiURL = $"https://api.polygon.io/v2/aggs/ticker/{StockTicker}/range/1/minute/2023-10-09/2023-11-09?adjusted=false&sort=asc&limit=50000&apiKey={APIKey}";
             return client.GetAsync(apiURL).Result;
